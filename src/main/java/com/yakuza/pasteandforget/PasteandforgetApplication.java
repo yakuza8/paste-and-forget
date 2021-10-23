cf8b7e6f9f2ef9e712e705c09e00ccbbd9026160
Berat Cankar <berat.cankar@udemy.com>
2021-10-23 16:44:37 +0300

    Project skeleton

---
+++ src/main/java/com/yakuza/pasteandforget/PasteandforgetApplication.java
@@ -0,0 +1,13 @@
+package com.yakuza.pasteandforget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteandforgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteandforgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yakuza/pasteandforget/PasteandforgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakuza.pasteandforget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteandforgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
