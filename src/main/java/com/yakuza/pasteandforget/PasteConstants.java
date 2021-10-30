26b00ce109cf1b62567f59a7cfc585f842b4ed9e
Berat Cankar <berat.cankar@udemy.com>
2021-10-30 21:25:53 +0300

    Add shortlink generator

--- src/main/java/com/yakuza/pasteandforget/PasteConstants.java
+++ src/main/java/com/yakuza/pasteandforget/PasteConstants.java
@@ -2,4 +2,5 @@ package com.yakuza.pasteandforget;
 
 public class PasteConstants {
     public static final Integer MAX_PASTE_SIZE = 1024;
+    public static final Integer MAX_SHORTLINK_LENGTH = 12;
 }
---
+++ src/main/java/com/yakuza/pasteandforget/common/ShortlinkGenerator.java
@@ -0,0 +1,45 @@
+package com.yakuza.pasteandforget.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.UUID;
+
+public class ShortlinkGenerator {
+
+    private final static String MD_ALGORITHM = "MD5";
+
+    private static Logger logger = LoggerFactory.getLogger(ShortlinkGenerator.class);
+
+    public static String generateShortlink() {
+        return base64(md5());
+    }
+
+    private static String md5() {
+        // Get random seed as time
+        Instant seed = Instant.now();
+        // But also add another randomness as applicable
+        UUID uuid = UUID.randomUUID();
+        // Combine them to be digested by md5
+        String shortlinkHash = uuid.toString() + seed;
+
+        try {
+            MessageDigest md = MessageDigest.getInstance(MD_ALGORITHM);
+            md.update(shortlinkHash.getBytes(), 0, shortlinkHash.length());
+            shortlinkHash = new BigInteger(1, md.digest()).toString();
+        } catch (NoSuchAlgorithmException e) {
+            logger.error("No such algorithm found as " + MD_ALGORITHM);
+        }
+        return shortlinkHash;
+    }
+
+    private static String base64(String shortlinkHash) {
+        return Base64.getUrlEncoder().encodeToString(shortlinkHash.getBytes());
+    }
+
+}
